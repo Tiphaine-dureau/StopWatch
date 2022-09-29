ba352898217b163b04464309e553f45dd5b85c7e
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-09-29 11:20:55 +0200

    Removed unused code

--- src/Controller/ChronoController.java
+++ src/Controller/ChronoController.java
@@ -1,33 +1,22 @@
 package Controller;
 
 import Model.ChronoModel;
-
-
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
 
 public class ChronoController extends JFrame {
     private JPanel container = new JPanel();
-    //Tableau stockant les éléments à afficher dans le Controller.ChronoController:
-    String[] arrayOfString = {"Start", "Lap", "Stop", "Resume", "Reset"};
-    //Un bouton par élément à afficher :
-    JButton[] jButtons = new JButton[arrayOfString.length];
     // 4 JLabel : 1 pour le chrono principal et 3 pour les 3 lap en dessous
     private JLabel[] screen = new JLabel[4];
-    //Cette variable stocke la dimension des boutons:
-    //taille des boutons
-    private Dimension dimension = new Dimension(90, 70); // TODO
     //Le SwingWorker qui va permettre de lancer le chrono en Background pour ne pas bloquer l'application
     private SwingWorker<Void, Integer> worker;
     private ChronoModel chronoModel;
 
 
     public ChronoController() {
-        this.setSize(350, 535);
+        this.setSize(350, 435);
         this.setTitle("Chronomètre");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
@@ -72,29 +61,6 @@ public class ChronoController extends JFrame {
             screen[i].setPreferredSize(new Dimension(300, 50));
             panScreen.add(screen[i]);
         }
-
-        //On parcourt le tableau initialisé afin de créer nos boutons avec le texte, la dimension et on les ajoute au sous conteneur
-        //TODO
-        for (int i = 0; i < arrayOfString.length; i++) {
-            jButtons[i] = new JButton(arrayOfString[i]);
-            jButtons[i].setPreferredSize(dimension);
-            buttonsPanel.add(jButtons[i]);
-            if (i != 0)
-                jButtons[i].setEnabled(false);
-        }
-
-        //On définit pour chaque bouton l'action Listener correspondant et une couleur
-        jButtons[0].addActionListener(new StartListener());
-        jButtons[0].setForeground(Color.RED);
-        jButtons[1].addActionListener(new LapListener());
-        jButtons[1].setForeground(Color.MAGENTA);
-        jButtons[2].addActionListener(new StopListener());
-        jButtons[2].setForeground(Color.BLUE);
-        jButtons[3].addActionListener(new ResumeListener());
-        jButtons[3].setForeground(Color.DARK_GRAY);
-        jButtons[4].addActionListener(new ResetListener());
-        jButtons[4].setForeground(Color.DARK_GRAY);
-
 
         //On ajoute après les deux sous conteneurs au conteneur principal container
         container.add(panScreen, BorderLayout.NORTH);
@@ -204,79 +170,5 @@ public class ChronoController extends JFrame {
                 return null;
             }
         };
-    }
-
-    // TODO Listeners dans Controller
-
-    //Listener affecté au bouton Start
-    class StartListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent arg0) {
-            chronoModel.setSavedTimeInMilliseconds(System.currentTimeMillis());
-            initializeWorker();
-            worker.execute();
-            jButtons[0].setEnabled(false);
-            jButtons[1].setEnabled(true);
-            jButtons[2].setEnabled(true);
-            jButtons[4].setEnabled(true);
-        }
-    }
-
-    // Listener affecté au bouton Lap
-    class LapListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent arg0) {
-            if (chronoModel.getLapClickCounter() == 3) {
-                jButtons[1].setEnabled(false);
-            }
-            screen[chronoModel.getLapClickCounter()].setText(chronoModel.getLapClickCounter() + ":" + String.format("%02d", chronoModel.getTimerModel().getHour()) + ":" + String.format("%02d", chronoModel.getTimerModel().getMinute()) + ":" + String.format("%02d", chronoModel.getTimerModel().getSecond()) + ":" + String.format("%02d", chronoModel.getTimerModel().getMillisecond()));
-            //screen[lap].paintImmediately(screen[lap].getVisibleRect()); ENLEVÉE CAR FAIT BUGUER LE VISUEL
-            chronoModel.setLapClickCounter(chronoModel.getLapClickCounter() + 1);
-        }
-    }
-
-    //Listener affecté au bouton Stop
-    class StopListener implements ActionListener {
-        public void actionPerformed(ActionEvent arg0) {
-            worker.cancel(true);
-            chronoModel.setSavedTimeInMillisecondsOnBreakStart(System.currentTimeMillis());
-            jButtons[2].setEnabled(false);
-            jButtons[3].setEnabled(true);
-        }
-    }
-
-    //Listener affecté au bouton Resume
-    class ResumeListener implements ActionListener {
-        public void actionPerformed(ActionEvent arg0) {
-            chronoModel.setSavedTimeInMillisecondsOnBreakEnd(System.currentTimeMillis());
-            initializeWorker();
-            worker.execute();
-            jButtons[2].setEnabled(true);
-            jButtons[3].setEnabled(false);
-        }
-    }
-
-    //Listener affecté au bouton Reset
-    class ResetListener implements ActionListener {
-        public void actionPerformed(ActionEvent arg0) {
-            worker.cancel(true);
-            chronoModel.getTimerModel().setHour(0);
-            chronoModel.getTimerModel().setMinute(0);
-            chronoModel.getTimerModel().setSecond(0);
-            chronoModel.getTimerModel().setMillisecond(0);
-            chronoModel.setSavedTimeInMillisecondsOnBreakStart(0);
-            chronoModel.setSavedTimeInMillisecondsOnBreakEnd(0);
-            chronoModel.setLapClickCounter(1);
-            for (int i = 0; i < 4; i++) {
-                if (i == 0) {
-                    screen[i].setText("00:00:00:00");
-                } else {
-                    screen[i].setText(i + " : 00:00:00:00");
-                }
-                jButtons[i].setEnabled(false);
-            }
-            jButtons[0].setEnabled(true);
-            jButtons[4].setEnabled(false);
-        }
     }
 }
